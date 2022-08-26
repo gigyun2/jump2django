0ecdeeccff3761045d648f83e1dd41fef6e26422
Gigyun Yoo <gigyun@naver.com>
2022-08-27 02:39:36 +0900

    [django] - change config to production - gather static file in static folder

--- jump2django/config/settings/prod.py
+++ jump2django/config/settings/prod.py
@@ -1,3 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['localhost']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []  # to prevent STATIC_ROOT be in _DIRS at base.py
+DEBUG = False
